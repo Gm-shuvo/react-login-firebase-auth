eb26ce6984bcdb5f86b33d7e33a9a1d80b591d11
gmshuvo <gm.shuvo.ru.cse@gmail.com>
2021-05-06 20:05:39 +0600

    fixed singin ui  ;)

--- src/Components/LogIn.jsx
+++ src/Components/LogIn.jsx
@@ -10,21 +10,23 @@ const LogIn = () => {
                     <Title>Sing in</Title>
                     <Paragraph>Access to 120+ hours of courses, tutorials and livestreams</Paragraph>
                     <FormInput>
-                        <EmailInput>
-                            <Icon >
+                        <Input>
+                           
                                 <img src="https://designcode.io/images/icons/envelope-blue.svg" alt="" />
-                            </Icon>
-                                
-                            <input type="email" placeholder="Email address" />
-                        </EmailInput>
-                        <PasswordInput>
+                         
                             
+                                <input type="email" placeholder="Email address" />
+                          
+                           
+                        </Input>
+                        <Input>
+                        <img src="https://designcode.io/images/icons/envelope-blue.svg" alt="" />
                             <input type="password" placeholder="Password" />
-                        </PasswordInput>
+                        </Input>
                        
                     </FormInput>
                     
-                    <SingInButton></SingInButton>
+                    <SingInButton onClick={''}>Sing In</SingInButton>
                     <Divider />
                     <SingUp></SingUp>
                     <ForgrtPass></ForgrtPass>
@@ -41,7 +43,7 @@ const Container = style.div`
     height:100vh;
     width:100vw;
     background: linear-gradient(20deg, rgba(46,209,210,1) 0%, rgba(50,255,159,1) 100%);
-    //background-filter:blur(20px)saturation(100%);
+    background-filter:blur(40px)saturation(70%);
     justify-content:center;
     align-items:center;
     z-index:10;
@@ -50,19 +52,21 @@ const Container = style.div`
 
 const Content = style.div`
     
-     position:relative;
-     max-width:960px;
-     max-height:600px;
-     display:grid;
-     grid-template-columns:2fr 1fr;
-     gap:40px;
-    padding: 20px 20px;
-    // place-content:center;
-     box-shadow: rgb(255 255 255 / 30%) 0px 0px 0px 0.5px inset;
-    // border-radius:20px;
-    // justify-items:center;
-
-     @media(max-width: 900px){
+    position:relative;
+    max-width:960px;
+    max-height:auto;
+    display:grid;
+    grid-template-columns:2fr .8fr;
+    gap:40px;
+    padding: 20px;
+     place-content:center;
+    background: rgb(71,187,174);
+    background: linear-gradient(34deg, rgba(71,187,174,0.6955824566154587) 0%, rgba(107,103,212,0.15216508966867992) 100%);
+    box-shadow: rgb(255 255 255 / 20%) 0px 0px 0px 0.7px inset;
+    border-radius:10px;
+    justify-items:center;
+    z-index:10;
+    @media(max-width: 900px){
          grid-template-columns: auto;
          margin:0 20px;
      }
@@ -78,7 +82,7 @@ const CoverImg = style.div`
     background-repeat:no-repeat;
     width:100%;
     height:100%;
-    border-radius:10px;
+    border-radius: 5px;
 
     img{
         max-width:100%;
@@ -90,11 +94,11 @@ const CoverImg = style.div`
 const SingInForm = style.div`
    
     display:grid;
-    
     justify-content:start;
-    //padding-top:75px;
-    max-width:320px;
-    //margin-bottom:70.5%;
+    padding-top:0px;
+    gap:20px;
+    //max-width:320px;
+   // margin-bottom:70.5%;
 
 `
 const Title = style.h1`
@@ -103,7 +107,7 @@ const Title = style.h1`
     font-sise:40px;
     margin:0;
     line-height:48px;
-    color:000000;
+    color:#000000;
 
 `
 const Paragraph = style.p`
@@ -111,35 +115,73 @@ const Paragraph = style.p`
     font-weight:normal;;
     font-size:15px;
     lineheight:130%;
-    color:rgb(255, 255, 255);
+    color:#000000;
 `
 const FormInput= style.form`
     display:grid;
-    gap:20px;
+    gap:12px;
+    margin-top:20px;
    
 `
 
-const EmailInput = style.div`
-    display;flex;
-    position:relative;
-    height:44px;
-    width:100%;
+const Input = style.div`
+
+    display:flex;
+    //justify-content:center;
+    align-items:center;
+    padding:0 10px;
+    gap:8px;
+    border:none;
+    
+    height:40px;
+    border-radius:20px;
+    background-color:rgba(255, 255, 255, 0.2);
+    //background:red;
+    input{
+        width:100%;
+        height:100%;
+        border:none;
+        outline:none;
+        
+        //margin-left:10px;
+        background-color:transparent;
+    }
+    img{
+        width:20px;
+        height:20px;
+        //display:block;
+       
+
+       
+        // position:absolute;
+        // top:0px;
+       // background-color:rgba(255, 255, 255, 0.5);
+        //border-radius: 50%;
+    }
+    
 `
 const Icon = style.div`
     width:36px;
     height:36px;
-    display:flex;
-    justify-content:center;
-    align-items:center;
-    position:absolute;
-    top:8px;
-    background-color:rgba(255, 255, 255, 0.2);
+    display:block;
+   
+    // position:absolute;
+    // top:0px;
+    background-color:rgba(255, 255, 255, 0.5);
     border-radius: 50%;
 
 `
 
-const PasswordInput= style.div``
-const SingInButton = style.button``
+
+const SingInButton = style.button`
+    height:34px;
+    width:100%
+    outline:none;
+    border:none;
+    border-radius:8px;
+    background: rgb(29,127,143);
+    background: linear-gradient(112deg, rgba(29,127,143,0.6091954022988506) 100%, rgba(0,255,174,0.23678160919540225) 100%);
+`
 const Divider = style.div``
 const SingUp = style.span``
 const ForgrtPass = style.span``
